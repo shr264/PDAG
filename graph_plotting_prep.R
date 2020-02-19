b934632d0d366200036105cb0b471dacab2ebfb6
Syed Rahman <syed.rahman@nielsen.com>
2020-02-19 17:28:36 -0500

    added new pdf for ti and no ti

--- graph_plotting_prep.R
+++ graph_plotting_prep.R
@@ -67,6 +67,82 @@ coords5 <- function(){
   return(coords)
 }
 
+coords5wTI <- function(){
+  coords = matrix(0,ncol = 2, nrow = 25)
+  num2 = 0
+  for (i in 1:6){
+    coords[i,2] = 8
+    coords[i,1] = 2*num2
+    num2 = num2 + 1
+  }
+  num2 = -0.75
+  for (i in 7:11){
+    coords[i,2] = 7
+    coords[i,1] = 2*num2
+    num2 = num2 + 1
+  }
+  
+  num2 = 0.75
+  for (i in 12:18){
+    coords[i,2] = 6
+    coords[i,1] = 2*num2
+    num2 = num2 + 1
+  }
+  
+  num2 = -1
+  for (i in 19:23){
+    coords[i,2] = 5
+    coords[i,1] = 2*num2
+    num2 = num2 + 1
+  }
+  
+  num2 = 1
+  for (i in 24:25){
+    coords[i,2] = 4
+    coords[i,1] = 2*num2
+    num2 = num2 + 1
+  }
+  return(coords)
+}
+
+coords5woTI <- function(){
+  coords = matrix(0,ncol = 2, nrow = 24)
+  num2 = 0
+  for (i in 1:6){
+    coords[i,2] = 8
+    coords[i,1] = 2*num2
+    num2 = num2 + 1
+  }
+  num2 = -0.75
+  for (i in 7:11){
+    coords[i,2] = 7
+    coords[i,1] = 2*num2
+    num2 = num2 + 1
+  }
+  
+  num2 = 0.75
+  for (i in 12:18){
+    coords[i,2] = 6
+    coords[i,1] = 2*num2
+    num2 = num2 + 1
+  }
+  
+  num2 = -1
+  for (i in 19:22){
+    coords[i,2] = 5
+    coords[i,1] = 2*num2
+    num2 = num2 + 1
+  }
+  
+  num2 = 1
+  for (i in 23:24){
+    coords[i,2] = 4
+    coords[i,1] = 2*num2
+    num2 = num2 + 1
+  }
+  return(coords)
+}
+
 coords8 <- function(){
   coords = matrix(0,ncol = 2, nrow = 30)
   num2 = 0
@@ -193,6 +269,138 @@ coords10 <- function(){
   num2 = 0
   for (i in 29:30){
     coords[i,2] = 1
+    coords[i,1] = 2*num2
+    num2 = num2 + 1
+  }
+  return(coords)
+}
+
+coords9wTI <- function(){
+  coords = matrix(0,ncol = 2, nrow = 25)
+  num2 = 0
+  for (i in 1:2){
+    coords[i,2] = 10
+    coords[i,1] = 2*num2
+    num2 = num2 + 1
+  }
+  num2 = -0.75
+  for (i in 3:3){
+    coords[i,2] = 9
+    coords[i,1] = 2*num2
+    num2 = num2 + 1
+  }
+  
+  num2 = -0.75
+  for (i in 4:4){
+    coords[i,2] = 8
+    coords[i,1] = 2*num2
+    num2 = num2 + 1
+  }
+  
+  num2 = -0.75
+  for (i in 5:6){
+    coords[i,2] = 7
+    coords[i,1] = 2*num2
+    num2 = num2 + 1
+  }
+  
+  num2 = 0.75
+  for (i in 7:9){
+    coords[i,2] = 6
+    coords[i,1] = 2*num2
+    num2 = num2 + 1
+  }
+  
+  num2 = -1
+  for (i in 10:11){
+    coords[i,2] = 5
+    coords[i,1] = 2*num2
+    num2 = num2 + 1
+  }
+  
+  num2 = 1
+  for (i in 12:18){
+    coords[i,2] = 4
+    coords[i,1] = 2*num2
+    num2 = num2 + 1
+  }
+  
+  num2 = -0.5
+  for (i in 19:23){
+    coords[i,2] = 3
+    coords[i,1] = 2*num2
+    num2 = num2 + 1
+  }
+  
+  num2 = 0.5
+  for (i in 24:25){
+    coords[i,2] = 2
+    coords[i,1] = 2*num2
+    num2 = num2 + 1
+  }
+  return(coords)
+}
+
+coords9woTI <- function(){
+  coords = matrix(0,ncol = 2, nrow = 24)
+  num2 = 0
+  for (i in 1:2){
+    coords[i,2] = 10
+    coords[i,1] = 2*num2
+    num2 = num2 + 1
+  }
+  num2 = -0.75
+  for (i in 3:3){
+    coords[i,2] = 9
+    coords[i,1] = 2*num2
+    num2 = num2 + 1
+  }
+  
+  num2 = -0.75
+  for (i in 4:4){
+    coords[i,2] = 8
+    coords[i,1] = 2*num2
+    num2 = num2 + 1
+  }
+  
+  num2 = -0.75
+  for (i in 5:6){
+    coords[i,2] = 7
+    coords[i,1] = 2*num2
+    num2 = num2 + 1
+  }
+  
+  num2 = 0.75
+  for (i in 7:9){
+    coords[i,2] = 6
+    coords[i,1] = 2*num2
+    num2 = num2 + 1
+  }
+  
+  num2 = -1
+  for (i in 10:11){
+    coords[i,2] = 5
+    coords[i,1] = 2*num2
+    num2 = num2 + 1
+  }
+  
+  num2 = 1
+  for (i in 12:18){
+    coords[i,2] = 4
+    coords[i,1] = 2*num2
+    num2 = num2 + 1
+  }
+  
+  num2 = -0.5
+  for (i in 19:22){
+    coords[i,2] = 3
+    coords[i,1] = 2*num2
+    num2 = num2 + 1
+  }
+  
+  num2 = 0.5
+  for (i in 23:24){
+    coords[i,2] = 2
     coords[i,1] = 2*num2
     num2 = num2 + 1
   }
